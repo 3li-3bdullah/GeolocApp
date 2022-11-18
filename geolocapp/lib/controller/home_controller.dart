d057c2f1686daf954aff43c424bbd4b1f02af7d0
3li-3bdullah <PROGRMMER7@gmail.com>
2022-11-18 10:54:56 +0200

    Added some methods

--- geolocapp/lib/controller/home_controller.dart
+++ geolocapp/lib/controller/home_controller.dart
@@ -1,12 +1,33 @@
+import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class HomeController extends GetxController{
+class HomeController extends GetxController {
   /// Declaring Variables 😀🔥
- BitmapDescriptor markerIcon = BitmapDescriptor.defaultMarker;
+  BitmapDescriptor markerIcon = BitmapDescriptor.defaultMarker;
+  bool isNormalPosition = true;
 
+  /// Declaring Mehods 😉🔥
+  
+  @override
+  onInit(){
+    super.onInit();
+    addCustomMarker();
+  }
+  
+  void addCustomMarker() {
+    BitmapDescriptor.fromAssetImage(
+           const ImageConfiguration(), 'assets/images/marker.png')
+        .then((icon) {
+          markerIcon = icon;
+          update();
+        });
+  }
 
- /// Declaring Mehods 😉🔥
- addCustomMarker(){}
+  void changePosition(){
+    isNormalPosition = !isNormalPosition;
+    update();
+  }
+
 
 }
