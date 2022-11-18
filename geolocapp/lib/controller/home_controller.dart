075d5620558c6078b50ae996c90d28b4f09b9cd4
3li-3bdullah <PROGRMMER7@gmail.com>
2022-11-18 10:19:23 +0200

    Created class home_controller and declared some vars and method

---
+++ geolocapp/lib/controller/home_controller.dart
@@ -0,0 +1,12 @@
+import 'package:get/get.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class HomeController extends GetxController{
+  /// Declaring Variables 😀🔥
+ BitmapDescriptor markerIcon = BitmapDescriptor.defaultMarker;
+
+
+ /// Declaring Mehods 😉🔥
+ addCustomMarker(){}
+
+}
